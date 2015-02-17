9dd42cd6c01b4178ec2a7fdcd821dffca32fd311
Alex H <hehe3301@gmail.com>
2015-02-17 15:54:05 -0500

    Build 1.2.0

--- src/main/java/lib/Constants.java
+++ src/main/java/lib/Constants.java
@@ -3,7 +3,7 @@ package lib;
 public class Constants {
 	public static final String MODID = "heheCompressionCraft";
 	public static final String MODNAME = " Hehe Compression Craft";
-	public static final String VERSION = "1.1.2";
+	public static final String VERSION = "1.2.0";
 	public static final String CLIENT_PROXY = "hehe.CompressionCraft.proxy.ClientProxy";
 	public static final String COMMON_PROXY = "hehe.CompressionCraft.proxy.CommonProxy";
 
